42ae3ef710ce83be3654c9827b91e3fee67f7c86
ArMKryuchkov <ArMKryuchkov@Greenatom.ru>
2023-11-28 17:29:03 +0300

    fix(env): hard code envs

--- hello_world/lib/neo4j.ts
+++ hello_world/lib/neo4j.ts
@@ -1,9 +1,9 @@
 import { driver, auth } from 'neo4j-driver'
 
 export const neo4jDriver = driver(
-    process.env.NEO4J_URI ?? '',
+    'bolt://neo4j:7687',
     auth.basic(
-        process.env.NEO4J_USERNAME ?? 'username',
-        process.env.NEO4J_PASSWORD ?? 'password'
+        'neo4j',
+        'testtesttest'
     )
 )
