28d6c99b9f003a16efae621d05a80e9f41bea96a
Katerina Pustovalova <51330147+Luthineth@users.noreply.github.com>
2023-11-29 20:41:52 +0300

    fix routes

---
+++ pages/experiments/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      hello
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
+
+<style scoped>
+
+</style>
