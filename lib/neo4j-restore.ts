9fd4cf5e6f0d94126e43aaa5d4f7432687e957a0
Mathless <mrkalumba@gmail.com>
2023-12-23 14:32:35 +0300

    fix(backend): restore edges

--- lib/neo4j-restore.ts
+++ lib/neo4j-restore.ts
@@ -4,36 +4,44 @@ import { neo4jDriver } from './neo4j';
 let driver = neo4jDriver;
 
 export async function restoreDatabase(data: {nodes: any, relations: any}): Promise<void> {
-    let session: Session = driver.session();
+    const session: Session = driver.session();
 
     try {
-        // Delete all existing nodes and relationships
         await session.run('MATCH (n) DETACH DELETE n');
 
-        // Create nodes
+        const nodeMappings: Record<number, any> = {};
+
         for (let node of data.nodes) {
-            let labels = node.labels.join(':');
-            let query = `CREATE (n:${labels} $properties) RETURN n`;
-            await session.run(query, {properties: node.properties});
+            const labels = node.labels.join(':');
+            const createNodeQuery = `CREATE (n:${labels} $properties) RETURN id(n) AS id`;
+            const result = await session.run(createNodeQuery, {properties: node.properties});
+            nodeMappings[node.identity] = result.records[0].get('id');
         }
 
-        // Create relationships
         for (let relation of data.relations) {
-            let query = `
-            MATCH (a:${relation.startNode.labels.join(':')}), (b:${relation.endNode.labels.join(':')})
-            WHERE a.name = $startNodeName AND b.name = $endNodeName
-            CREATE (a)-[:${relation.relationship.type} $properties]->(b)
+            if (!relation.startNode || !relation.endNode) {
+                console.error('Invalid relation object: missing startNode or endNode');
+                continue;
+            }
+
+            const createRelationQuery = `
+                MATCH (a)
+                WHERE id(a) = $startNodeIdentity
+                MATCH (b)
+                WHERE id(b) = $endNodeIdentity
+                CREATE (a)-[:${relation.relationship.type} $properties]->(b)
             `;
 
-            await session.run(query, {
-                startNodeName: relation.startNode.properties.name,
-                endNodeName: relation.endNode.properties.name,
-                properties: relation.relationship.properties
+            await session.run(createRelationQuery, {
+                startNodeIdentity: nodeMappings[relation.startNode.identity],
+                endNodeIdentity: nodeMappings[relation.endNode.identity],
+                properties: relation.relationship.properties,
             });
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     } finally {
         session.close();
     }
 }
+
